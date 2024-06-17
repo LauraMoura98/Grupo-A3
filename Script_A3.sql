83e603c99ff284d19ba2d7771bc8bde86d896dcf
lauramoura98 <lauramoura06@gmail.com>
2024-06-17 20:00:15 -0300

    add sql script

---
+++ Script_A3.sql
@@ -0,0 +1,13 @@
+SELECT * FROM tb_user 
+
+INSERT INTO tb_user (id, nome, email, password)
+VALUES (1, "Laura Moura Silva", "lauramoura06@gmail.com", "techlead");
+
+INSERT INTO tb_user (id, nome, email, password)
+VALUES (2, "Heitor Rodrigues de Sousa", "heitor.rodrigues3010@gmail.com", "requisitos");
+
+INSERT INTO tb_user (id, nome, email, password)
+VALUES (3, "Iago Costa e Silva", "iagocosta@gmail.com", "backend");
+
+INSERT INTO tb_user (id, nome, email, password)
+VALUES (4, "Kelvin Marlon", "kelvinmarlon@gmail.com", "database");
